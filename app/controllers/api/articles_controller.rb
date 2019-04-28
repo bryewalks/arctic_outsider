3b826b0529fefa2037ae3e4ffc5820e73fef21c3
bryewalks <bryewalks@gmail.com>
2019-04-28 13:04:36 -0500

    initial commit

---
+++ app/controllers/api/articles_controller.rb
@@ -0,0 +1,47 @@
+class Api::ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+    render 'index.json.jbuilder'
+  end
+
+  def create
+    @article = Article.new(
+                              title: params[:title],
+                              body: params[:body],
+                              category: params[:category],
+                              user_id: params[:user_id]
+                              )
+    
+    if @article.save
+      render 'show.json.jbuilder'
+    else
+      render json: {errors: @article.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
+    render 'show.json.jbuilder'
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    @article.title = params[:title] || @article.title
+    @article.body = params[:body] || @article.body
+    @article.category = params[:category] || @article.category
+    @article.user_id = params[:user_id] || @article.user_id
+
+    if @article.save
+      render 'show.json.jbuilder'
+    else
+      render json: {errors: @article.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    article = Article.find(params[:id])
+    article.destroy
+    render json: {message: "Successfully removed article."}
+  end
+end
---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,45 @@
+class Api::CommentsController < ApplicationController
+  def index
+    @comments = Comment.all
+    render 'index.json.jbuilder'
+  end
+
+  def create
+    @comment = Comment.new(
+                              body: params[:body],
+                              user_id: params[:user_id],
+                              article_id: params[:article_id],
+                              )
+    
+    if @comment.save
+      render 'show.json.jbuilder'
+    else
+      render json: {errors: @comment.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @comment = Comment.find(params[:id])
+    render 'show.json.jbuilder'
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+
+    @comment.body = params[:body] || @comment.body
+    @comment.user_id = params[:user_id] || @comment.user_id
+    @comment.article_id = params[:article_id] || @comment.article_id
+
+    if @comment.save
+      render 'show.json.jbuilder'
+    else
+      render json: {errors: @comment.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
+    render json: {message: "Successfully removed comment."}
+  end
+end
