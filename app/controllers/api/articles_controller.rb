68e772bfde46dad052bb8e63ac499ccab8a08632
bryewalks <bryewalks@gmail.com>
2019-06-08 14:51:59 -0500

    added user to seeds file

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -1,6 +1,5 @@
 class Api::ArticlesController < ApplicationController
-  before_action :authenticate_admin, except: [:index, :show]
-
+  # before_action :authenticate_admin, except: [:index, :show]
   def index
     @articles = Article.all
     render 'index.json.jbuilder'
